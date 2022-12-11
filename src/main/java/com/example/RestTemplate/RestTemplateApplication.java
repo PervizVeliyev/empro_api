107098466319b1f67eb73de6d46c04c609f5876a
PervizVeliyev <veliyevperviz1@gmail.com>
2022-12-11 22:30:46 +0400

    second commit

--- src/main/java/com/example/RestTemplate/RestTemplateApplication.java
+++ src/main/java/com/example/RestTemplate/RestTemplateApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.*;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.*;
@@ -31,20 +29,14 @@ public class RestTemplateApplication implements CommandLineRunner {
 					headers.setContentType(MediaType.APPLICATION_JSON);
 					headers.set("ACCESS-KEY", "Moodle deea5cf5-bb04-4394-a2a4-d3d0fb3e1f1d-abcd14b1-133b-4121-88b1-e779a2f2f106");
 					List<Student> students = new ArrayList<>();
-					List<HttpMessageConverter<?>> messageConverters = new ArrayList<>();
-					MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
 
-					converter.setSupportedMediaTypes(Collections.singletonList(MediaType.ALL));
-					messageConverters.add(converter);
-					restTemplate.setMessageConverters(messageConverters);
+					students.add(new Student("ran@mail.ru", "Eliyev Eli Veli", "09/11/2011", "10","math"));
+					students.add(new Student("rndom@gmail.com", "Eliyev Eli Veli", "01/10/2012", "10","math"));
 
-					students.add(new Student("random@mail.ru", "Eliyev Eli Veli", "11/11/2011", "10","math"));
-					students.add(new Student("random@gmail.com", "Eliyev Eli Veli", "11/10/2012", "10","math"));
-
-					String json = new ObjectMapper().writeValueAsString(students);
+					String json = "{\"students\":" + new ObjectMapper().writeValueAsString(students) + "}";
 					HttpEntity<String> entity = new HttpEntity<>(json, headers);
 
-					ResponseEntity<String> res = restTemplate.exchange(postUrl, HttpMethod.POST, entity, String.class);
+					ResponseEntity<Object> res = restTemplate.exchange(postUrl, HttpMethod.POST, entity, Object.class);
 					System.out.println(res.getStatusCode());
 					System.out.println(res);
 
